a9d3395b9665ad4e32bf0d38b3bbb4d42fee0e8a
GGGGGHT <wz9712203617@gmail.com>
2022-08-25 21:18:58 +0800

    Create DirectoryMemory.java

---
+++ src/DirectoryMemory.java
@@ -0,0 +1,13 @@
+public class DirectoryMemory {
+  public static void main(String[] args) {
+    Class c = java.nio.Bits.class;
+    Field maxMemory = c.getDeclaredField("maxMemory");
+    maxMemory.setAccessible(true);
+    Field reservedMemory = c.getDeclaredField("reservedMemory");
+    reservedMemory.setAccessible(true);
+    synchronized (c) {
+        Long maxMemoryValue = (Long)maxMemory.get(null);
+        Long reservedMemoryValue = (Long)reservedMemory.get(null);
+    }
+  }
+}
